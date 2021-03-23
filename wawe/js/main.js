06a906c2639292964bd86ce7788ca13086514dc8
Alexander Gvozdev <gvozdev-work@mail.ru>
2021-03-23 22:44:36 +0700

    [Wawe] аддаптив галлереи

--- wawe/js/main.js
+++ wawe/js/main.js
@@ -12,8 +12,6 @@ $(function () {
 
   $('.menu__btn').on('click', function () {
     $('.header__top').toggleClass('header__top--active');
-
-    $('.menu__btn').toggleClass('menu__btn--active');
   })
 
   mixitup('.gallery__images');
