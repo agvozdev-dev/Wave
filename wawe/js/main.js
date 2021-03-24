045b1c73ae0615da480037ade092e02c5ba04b2d
Alexander Gvozdev <gvozdev-work@mail.ru>
2021-03-24 20:04:43 +0700

    [Wawe] реализация плавного скролла

--- wawe/js/main.js
+++ wawe/js/main.js
@@ -14,5 +14,15 @@ $(function () {
     $('.header__top').toggleClass('header__top--active');
   })
 
+	$('.menu a').on('click', function (event) {
+		event.preventDefault();
+    
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+
+
   mixitup('.gallery__images');
 })
