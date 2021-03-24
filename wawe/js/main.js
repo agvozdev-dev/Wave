64a0d3fbda8445a35e674ff8b7d37c0bb9c74aef
Alexander Gvozdev <gvozdev-work@mail.ru>
2021-03-24 20:41:44 +0700

    [Wawe] background для липкого скрола

--- wawe/js/main.js
+++ wawe/js/main.js
@@ -10,19 +10,26 @@ $(function () {
     arrows: false,
   });
 
-  $('.menu__btn').on('click', function () {
+  $('.menu__btn, .menu__link').on('click', function () {
     $('.header__top').toggleClass('header__top--active');
   })
 
-	$('.menu a, .header__icon').on('click', function (event) {
-		event.preventDefault();
+  $('.menu a, .header__icon').on('click', function (event) {
+    event.preventDefault();
 
-		var id  = $(this).attr('href'),
-		top = $(id).offset().top;
-		
-		$('body,html').animate({ scrollTop: top }, 1500);
-	});
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
 
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $(window).scroll(function (event) {
+    if ($(window).scrollTop() > 50) {
+      $(".header__top").addClass("header__top--bg-gray");
+    } else {
+      $(".header__top").removeClass("header__top--bg-gray");
+    }
+  });
 
   mixitup('.gallery__images');
 })
